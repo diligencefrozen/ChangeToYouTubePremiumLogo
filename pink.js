a172919e8ad509a0dcdf24f4b71bc1fa0bf21018
Frozen <152419439+diligencefrozen@users.noreply.github.com>
2024-01-08 20:17:27 +0800

    Create pink.js

---
+++ pink.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         FakeYTPremiumlogo(Pink)
+
+// @namespace    http://tampermonkey.net/
+
+// @version      20240108.3
+
+// @description  Change YouTube logo to Premium version
+
+// @author       diligencefrozen
+
+// @match        https://www.youtube.com/*
+
+// @grant        none
+
+
+// ==/UserScript==
+
+function changeYouTubeLogo() {
+    var ytIcon = document.getElementById('logo-icon');
+    ytIcon.innerHTML = '';
+
+    var newLogo = new Image();
+    newLogo.width = 94;
+
+    if (document.documentElement.getAttribute('dark')) {
+        newLogo.src = 'https://github.com/diligencefrozen/FakeYTPremiumlogo/blob/main/logo/logov5.png?raw=true';
+    } else {
+        newLogo.src = 'https://github.com/diligencefrozen/FakeYTPremiumlogo/blob/main/logo/logov6.png?raw=true';
+    }
+
+    ytIcon.appendChild(newLogo);
+}
+
+changeYouTubeLogo();
