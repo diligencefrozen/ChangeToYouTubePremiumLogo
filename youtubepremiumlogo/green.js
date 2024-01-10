b2b4a100f8bb9d9b0a0eef43d771548750161544
Frozen <152419439+diligencefrozen@users.noreply.github.com>
2024-01-10 11:25:42 +0800

    Create green.js

---
+++ youtubepremiumlogo/green.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         ChangeToYouTubePremiumLogo(Green)
+
+// @namespace    http://tampermonkey.net/
+
+// @version      20240110.1
+
+// @description  Change YouTube logo to Premium version
+
+// @author       diligencefrozen
+
+// @match        https://www.youtube.com/*
+
+// @grant        none
+
+
+// ==/UserScript==
+
+function changeYouTubeLogo() {
+    var ytIcon = document.getElementById('logo-icon');
+    ytIcon.innerHTML = '';
+
+    var newLogo = new Image();
+    newLogo.width = 94;
+
+    if (document.documentElement.getAttribute('dark')) {
+        newLogo.src = 'https://github.com/diligencefrozen/ChangeToYouTubePremiumLogo/blob/main/logo/logov9.png?raw=true';
+    } else {
+        newLogo.src = 'https://github.com/diligencefrozen/ChangeToYouTubePremiumLogo/blob/main/logo/logov10.png?raw=true';
+    }
+
+    ytIcon.appendChild(newLogo);
+}
+
+changeYouTubeLogo();
