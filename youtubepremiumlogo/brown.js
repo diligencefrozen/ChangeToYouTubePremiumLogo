bc7d9d0eb51439d2b98abfe3dc120c02266b46d8
Frozen <152419439+diligencefrozen@users.noreply.github.com>
2024-01-11 23:02:43 +0800

    Create brown.js

---
+++ youtubepremiumlogo/brown.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         ChangeToYouTubePremiumLogo(Brown)
+
+// @namespace    http://tampermonkey.net/
+
+// @version      20240112.1
+
+// @description  Change YouTube logo to Premium version
+
+// @author       diligencefrozen
+
+// @match        https://www.youtube.com/*
+
+// @grant        none
+
+
+// ==/UserScript==
+
+function changeYouTubeLogo() {
+    var ytIcon = document.getElementById('logo-icon');
+    ytIcon.innerHTML = '';
+
+    var newLogo = new Image();
+    newLogo.width = 94;
+
+    if (document.documentElement.getAttribute('dark')) {
+        newLogo.src = 'https://github.com/diligencefrozen/ChangeToYouTubePremiumLogo/blob/main/logo/logov11.png?raw=true';
+    } else {
+        newLogo.src = 'https://github.com/diligencefrozen/ChangeToYouTubePremiumLogo/blob/main/logo/logov12.png?raw=true';
+    }
+
+    ytIcon.appendChild(newLogo);
+}
+
+changeYouTubeLogo();
