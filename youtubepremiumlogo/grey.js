c26486dd1eebb4338b39f38dd0f829ad9360e94a
Frozen <152419439+diligencefrozen@users.noreply.github.com>
2024-01-20 23:33:08 +0900

    Update grey.js

--- youtubepremiumlogo/grey.js
+++ youtubepremiumlogo/grey.js
@@ -1,41 +1,36 @@
 // ==UserScript==
 
 // @name         ChangeToYouTubePremiumLogo(Grey)
-
-// @version      20240120.5
-
+// @version      20240120.11
 // @description  Change YouTube logo to Premium version
-
 // @author       diligencefrozen
-
 // @match        https://www.youtube.com/*
-
 // @grant        none
-
 // @namespace    https://github.com/diligencefrozen/ChangeToYouTubePremiumLogo
-
 // @icon         https://www.google.com/s2/favicons?domain=youtube.com
-
 // @updateURL    https://raw.githubusercontent.com/diligencefrozen/ChangeToYouTubePremiumLogo/main/youtubepremiumlogo/grey.js
-
 // @downloadURL  https://raw.githubusercontent.com/diligencefrozen/ChangeToYouTubePremiumLogo/main/youtubepremiumlogo/grey.js
 
 // ==/UserScript==
 
-function changeYouTubeLogo() {
-    var ytIcon = document.getElementById('logo-icon');
-    ytIcon.innerHTML = '';
+(function() {
+    'use strict';
+    // jQuery를 충돌 없이 사용하기 위한 설정
+    const $ = jQuery.noConflict(true);
 
-    var newLogo = new Image();
-    newLogo.width = 94;
+    // 로고의 위치를 선택하고 기존 내용을 비움
+    let logoContainer = $('#logo-icon').empty();
 
-    if (document.documentElement.getAttribute('dark')) {
-        newLogo.src = 'https://github.com/diligencefrozen/ChangeToYouTubePremiumLogo/blob/main/logo/logov14.png?raw=true';
-    } else {
-        newLogo.src = 'https://github.com/diligencefrozen/ChangeToYouTubePremiumLogo/blob/main/logo/logov13.png?raw=true';
-    }
+    // 적용할 로고 이미지의 URL 설정
+    let darkModeLogoURL = 'https://github.com/diligencefrozen/ChangeToYouTubePremiumLogo/blob/main/logo/logov13.png?raw=true';
+    let lightModeLogoURL = 'https://github.com/diligencefrozen/ChangeToYouTubePremiumLogo/blob/main/logo/logov14.png?raw=true';
 
-    ytIcon.appendChild(newLogo);
-}
+    // 다크 모드 여부에 따라 적절한 로고 적용
+    let logoURL = $('html').is('[dark]') ? darkModeLogoURL : lightModeLogoURL;
 
-changeYouTubeLogo();
+    // 이미지 요소 생성 및 스타일 설정
+    let imgElement = $('<img>', { src: logoURL, style: 'width: 94px' });
+
+    // 로고 컨테이너에 이미지 삽입
+    logoContainer.append(imgElement);
+})();
