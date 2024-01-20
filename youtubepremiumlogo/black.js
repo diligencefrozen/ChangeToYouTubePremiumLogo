514b8fe7b29b6e59fb37741ada48c1c75932e15d
Frozen <152419439+diligencefrozen@users.noreply.github.com>
2024-01-20 21:35:27 +0900

    Update black.js

--- youtubepremiumlogo/black.js
+++ youtubepremiumlogo/black.js
@@ -1,24 +1,14 @@
 // ==UserScript==
 // @name         ChangeToYouTubePremiumLogo(Black)
-
-// @version      20240120.4
-
+// @version      20240120.5
 // @description  Change YouTube logo to Premium version
-
 // @author       diligencefrozen
-
 // @match        https://www.youtube.com/*
-
 // @grant        none
-
 // @namespace    https://github.com/diligencefrozen/ChangeToYouTubePremiumLogo
-
 // @icon         https://www.google.com/s2/favicons?domain=youtube.com
-
 // @updateURL    https://raw.githubusercontent.com/diligencefrozen/ChangeToYouTubePremiumLogo/main/youtubepremiumlogo/black.js
-
 // @downloadURL  https://raw.githubusercontent.com/diligencefrozen/ChangeToYouTubePremiumLogo/main/youtubepremiumlogo/black.js
-
 // ==/UserScript==
 
 function changeYouTubeLogo() {
