29cd42ab514d2400a2322d93f3d322819d5ced94
Frozen <152419439+diligencefrozen@users.noreply.github.com>
2024-01-20 20:54:32 +0900

    Update black.js

--- youtubepremiumlogo/black.js
+++ youtubepremiumlogo/black.js
@@ -1,9 +1,7 @@
 // ==UserScript==
 // @name         ChangeToYouTubePremiumLogo(Black)
 
-// @namespace    http://tampermonkey.net/
-
-// @version      20240120.2
+// @version      20240120.3
 
 // @description  Change YouTube logo to Premium version
 
