f9d129aabe0a55cf80a180bf461bd00c08d4fc5e
Frozen <152419439+diligencefrozen@users.noreply.github.com>
2024-01-20 21:27:42 +0900

    Update green.js

--- youtubepremiumlogo/green.js
+++ youtubepremiumlogo/green.js
@@ -13,6 +13,12 @@
 
 // @namespace    https://github.com/diligencefrozen/ChangeToYouTubePremiumLogo
 
+// @icon         https://www.google.com/s2/favicons?domain=youtube.com
+
+// @updateURL    https://raw.githubusercontent.com/diligencefrozen/ChangeToYouTubePremiumLogo/main/youtubepremiumlogo/black.js
+
+// @downloadURL  https://raw.githubusercontent.com/diligencefrozen/ChangeToYouTubePremiumLogo/main/youtubepremiumlogo/black.js
+
 // ==/UserScript==
 
 function changeYouTubeLogo() {
