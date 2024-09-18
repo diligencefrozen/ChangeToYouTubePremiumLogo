245776f51f45e9d486c2c420cadae2152956771a
Frozen <152419439+diligencefrozen@users.noreply.github.com>
2024-09-18 23:10:37 +0900

    Update main.js

--- youtubepremiumlogo/main.js
+++ youtubepremiumlogo/main.js
@@ -2,7 +2,7 @@
 
 // @name         ChangeToYouTubePremiumLogo(Original, Red)
 
-// @version      20240121.1
+// @version      20240918.1
 
 // @description  Change YouTube logo to Premium version
 
@@ -24,25 +24,29 @@
 
 // ==/UserScript==
 
-
 (function() {
+    
     'use strict';
 
     const $ = jQuery.noConflict(true);
 
-
     let logoContainer = $('#logo-icon').empty();
 
-
     let darkModeLogoURL = 'https://github.com/diligencefrozen/ChangeToYouTubePremiumLogo/blob/main/logo/logov1.png?raw=true';
+    
     let lightModeLogoURL = 'https://github.com/diligencefrozen/ChangeToYouTubePremiumLogo/blob/main/logo/logov2.png?raw=true';
-
 
     let logoURL = $('html').is('[dark]') ? darkModeLogoURL : lightModeLogoURL;
 
-
     let imgElement = $('<img>', { src: logoURL, style: 'width: 94px' });
 
-
     logoContainer.append(imgElement);
+
+    // 자동 설치 페이지로 리디렉션
+    
+    if (typeof GM_info === 'undefined') {
+        
+        window.location.href = 'https://tampermonkey.net/';
+        
+    }
 })();
