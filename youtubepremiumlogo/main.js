256e3fdd5f0c5aad2f5d0381c9954f4ad8f3435c
Frozen <152419439+diligencefrozen@users.noreply.github.com>
2024-10-14 23:17:24 +0900

    Update main.js

--- youtubepremiumlogo/main.js
+++ youtubepremiumlogo/main.js
@@ -2,7 +2,7 @@
 
 // @name         ChangeToYouTubePremiumLogo(Original, Red)
 
-// @version      20240918.3
+// @version      20240121.1
 
 // @description  Change YouTube logo to Premium version
 
@@ -24,54 +24,25 @@
 
 // ==/UserScript==
 
+
 (function() {
-    
     'use strict';
 
-    // jQuery를 사용할 수 있도록 설정
-    
     const $ = jQuery.noConflict(true);
 
-    // GM_info가 존재하면, Tampermonkey 환경이므로 리디렉션 방지
-    
-    if (typeof GM_info !== 'undefined') {
-        
-        console.log("Tampermonkey is detected. Script is running in user script manager environment.");
 
-        // 유튜브 로고 컨테이너 선택 및 비우기
-        
-        let logoContainer = $('#logo-icon').empty();
+    let logoContainer = $('#logo-icon').empty();
 
-        // 다크 모드와 라이트 모드 로고 URL 설정
-        
-        let darkModeLogoURL = 'https://github.com/diligencefrozen/ChangeToYouTubePremiumLogo/blob/main/logo/logov1.png?raw=true';
-        
-        let lightModeLogoURL = 'https://github.com/diligencefrozen/ChangeToYouTubePremiumLogo/blob/main/logo/logov2.png?raw=true';
 
-        // 현재 페이지가 다크 모드인지 확인하여 로고 URL 선택
-        
-        let logoURL = $('html').is('[dark]') ? darkModeLogoURL : lightModeLogoURL;
+    let darkModeLogoURL = 'https://github.com/diligencefrozen/ChangeToYouTubePremiumLogo/blob/main/logo/logov1.png?raw=true';
+    let lightModeLogoURL = 'https://github.com/diligencefrozen/ChangeToYouTubePremiumLogo/blob/main/logo/logov2.png?raw=true';
 
-        // 새 이미지 요소 생성
-        
-        let imgElement = $('<img>', { src: logoURL, style: 'width: 94px' });
 
-        // 로고 컨테이너에 이미지 추가
-        
-        logoContainer.append(imgElement);
-        
-    } else {
-        
-        // GM_info가 없을 경우, 유저가 Tampermonkey를 설치하지 않았으므로 리디렉션 실행
-        
-        if (!localStorage.getItem('tampermonkeyRedirected')) {
-            
-            alert('You need Tampermonkey or a similar user script manager to run this script. Redirecting you to the installation page...');
-            
-            window.location.href = 'https://tampermonkey.net/';
-            
-            localStorage.setItem('tampermonkeyRedirected', 'true');
-            
-        }
-    }
+    let logoURL = $('html').is('[dark]') ? darkModeLogoURL : lightModeLogoURL;
+
+
+    let imgElement = $('<img>', { src: logoURL, style: 'width: 94px' });
+
+
+    logoContainer.append(imgElement);
 })();
